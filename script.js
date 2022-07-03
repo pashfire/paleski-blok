708866d8c48b0a33a5cd9058c0d7df4ed0f67669
pashfire <pashfire@gmail.com>
2022-07-04 01:00:23 +0300

    move js code into script.js file

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 "use strict";
-import {burgerMenu} from './src/js/_burger.js';
+
+const burgerMenu = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.header-navigation__wrapper');
+ 
+  //Toggle Nav
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('nav-active');
+  });  
+};
 
 burgerMenu();
